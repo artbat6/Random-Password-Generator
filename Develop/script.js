ac4979da554c15a95625ed415c9702ce01806e7b
artbat6 <arthurbatesdev@gmail.com>
2021-02-28 14:15:05 -0600

    completed for loop

--- Develop/script.js
+++ Develop/script.js
@@ -24,8 +24,7 @@ function generatePassword() {
   }
 
   var responseArray = askQuestions();
-  {
-  }
+
   // askQuestions(); this should populate responses
   // if responsesArray is empty... show a message and repeat (18-22)
 
@@ -35,101 +34,119 @@ function generatePassword() {
       responseArray = askQuestions();
     }
   }
-  // else if the array is not empty... move on - to random number generation
-}
-function askQuestions() {
-  const responses = [];
 
-  const upperQ = confirm("Do you want uppercase letters?");
-  const lowerQ = confirm("Do you want lowercase letters?");
-  const numberQ = confirm("Do you want numbers?");
-  const symbolQ = confirm("Do you want symbols?");
-
-  console.log(responses.includes("upperQ"));
-
-  //instead of pushing to an array do individual function calling
-
-  if (upperQ === true) {
-    responses.push("upperQ");
-    console.log("include uppercase letters");
-  } else {
-    console.log("* no uppercase");
+  // if (responseArray.includes("upperQ")) {
+  //   var randomUpper = getRandomUpper();
+  // }
+  //take length variable run a for loop for that number
+  //8 chars loop through response up to eight
+  var i;
+  var finalPassword = "";
+  for (i = 0; i <= passwordLength; i++) {
+    if (responseArray.includes("upperQ")) {
+      var randomUpper = getRandomUpper();
+      finalPassword += randomUpper;
+    }
+    if (responseArray.includes("lowerQ")) {
+      var randomLower = getRandomLower();
+      finalPassword += randomLower;
+    }
+    if (responseArray.includes("numberQ")) {
+      var randomNumber = getRandomNumber();
+      finalPassword += randomNumber;
+    }
+    if (responseArray.includes("symbolQ")) {
+      var randomSymbol = getRandomSymbol();
+      finalPassword += randomSymbol;
+    }
+    console.log(finalPassword);
   }
-  if (lowerQ === true) {
-    // push into array
-    responses.push("lowerQ");
-  } else {
-    console.log("* no lowercase");
+  return finalPassword;
+
+  function askQuestions() {
+    const responses = [];
+
+    const upperQ = confirm("Do you want uppercase letters?");
+    const lowerQ = confirm("Do you want lowercase letters?");
+    const numberQ = confirm("Do you want numbers?");
+    const symbolQ = confirm("Do you want symbols?");
+
+    console.log(responses.includes("upperQ"));
+
+    //instead of pushing to an array do individual function calling
+
+    if (upperQ === true) {
+      responses.push("upperQ");
+      console.log("include uppercase letters");
+    } else {
+      console.log("* no uppercase");
+    }
+    if (lowerQ === true) {
+      // push into array
+      responses.push("lowerQ");
+    } else {
+      console.log("* no lowercase");
+    }
+    if (numberQ === true) {
+      // push into array
+      responses.push("numberQ");
+    } else {
+      console.log("* no numbers");
+    }
+    if (symbolQ === true) {
+      // push into array
+      responses.push("symbolQ");
+    } else {
+      console.log("* no symbols");
+    }
+    console.log(responses);
+    return responses;
   }
-  if (numberQ === true) {
-    // push into array
-    responses.push("numberQ");
-  } else {
-    console.log("* no numbers");
+
+  //----------------------------------------------------------------//
+  // take the approved categories and generate randoms of each ategory
+
+  // for loop through the chosen character types for the length of passwordLength
+
+  //---------------------------------------------------------------//
+
+  function checkLength(passwordLength) {
+    // if the length is a number && >=8 && <= 128 return true
+    if (
+      !isNaN(passwordLength) &&
+      passwordLength >= 8 &&
+      passwordLength <= 128
+    ) {
+      console.log("this IS valid");
+      return true;
+    } else {
+      console.log("this is not valid input");
+      return false;
+    }
   }
-  if (symbolQ === true) {
-    // push into array
-    responses.push("symbolQ");
-  } else {
-    console.log("* no symbols");
+  //------------------------------------------------------------------//
+
+  //---------------------------------------------------------------//
+  //****************************************************** */
+
+  //******************************************************* */
+
+  //---------------------------------------------------------------//
+
+  function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
   }
-  console.log(responses);
-  return responses;
-}
 
-//----------------------------------------------------------------//
-// take the approved categories and generate randoms of each ategory
-
-// for loop through the chosen character types for the length of passwordLength
-
-//if (responses[0] === true) {
-//console.log("this is where the generate function would be called");
-//}
-
-//if (responses[1] === true) {
-//}
-
-//---------------------------------------------------------------//
-
-function checkLength(passwordLength) {
-  // if the length is a number && >=8 && <= 128 return true
-  if (!isNaN(passwordLength) && passwordLength >= 8 && passwordLength <= 128) {
-    console.log("this IS valid");
-    return true;
-  } else {
-    console.log("this is not valid input");
-    return false;
+  function getRandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
   }
-}
-//------------------------------------------------------------------//
 
-//---------------------------------------------------------------//
-//****************************************************** */
-//putting all of the getRandom functions into an object called randomFunc
-// var randomFunc = {
-//   upper: getRandomUpper,
-//   lower: getRandomUpper,
-//   number: getRandomNumber,
-//   symbol: getRandomSymbol,
-// };
+  function getRandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+  }
 
-//******************************************************* */
-
-//---------------------------------------------------------------//
-
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-function getRandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function getRandomSymbol() {
-  const symbols = "!@#$%^&*(){}[]=<>/,.";
-  return symbols[Math.floor(Math.random() * symbols.length)];
+  function getRandomSymbol() {
+    const symbols = "!@#$%^&*(){}[]=<>/,.";
+    return symbols[Math.floor(Math.random() * symbols.length)];
+  }
 }
